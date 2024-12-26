0626ccdf69fc21062193e5a32db1b421a39d002a
inam95 <mfihassan99@gmail.com>
2024-12-26 08:57:50 +0530

    initial setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import tailwindTypography from "@tailwindcss/typography";
 
 export default {
   content: [
@@ -14,5 +15,5 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [tailwindTypography],
 } satisfies Config;
